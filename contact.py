7215e34567bf136b29a46e543d76c1f53a511092
akasimova <brskasimova@gmail.com>
2020-12-05 23:01:07 +0300

    Замена параметров объектом в тесте добавления контакта

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, mobilephone):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.mobilephone = mobilephone
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
@@ -12,7 +13,7 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, firstname="testfirstname", middlename="testmiddlename", lastname="testlastname", mobilephone="70000000000")
+        self.create_contact(wd, Contact(firstname="testfirstname", middlename="testmiddlename", lastname="testlastname", mobilephone="70000000000"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -20,7 +21,7 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, firstname="", middlename="", lastname="", mobilephone="")
+        self.create_contact(wd, Contact(firstname="", middlename="", lastname="", mobilephone=""))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -30,20 +31,20 @@ class TestAddContact(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_contact(self, wd, firstname, middlename, lastname, mobilephone):
+    def create_contact(self, wd, contact):
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobilephone)
+        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
