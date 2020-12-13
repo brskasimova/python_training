415534976a4a86925a6139a4d85fb6915b1ab4c9
akasimova <brskasimova@gmail.com>
2020-12-13 18:20:34 +0300

    Добавлен тест редактирования группы

--- fixture/group.py
+++ fixture/group.py
@@ -33,8 +33,30 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        #select first group
+        # select first group
         wd.find_element_by_name("selected[]").click()
-        #submit deletion
+        # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # group edit init
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group update
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
