bc58239faca3de0cc6cf6dee5cd350510d861d05
akasimova <brskasimova@gmail.com>
2021-01-09 13:59:54 +0300

    Реализация генератора тестовых данных, сохраняющего данные в json файл, загрузка тестовых данных из файла для групп

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+testdata = [
+    Group(name="testname1", header="testheader1", footer="testfooter1"),
+    Group(name="testname2", header="testheader2", footer="testfooter2")
+]
